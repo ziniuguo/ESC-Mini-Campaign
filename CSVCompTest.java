ca80e9462660f1e120e085a403379530c7984def
Ziniu <guo.ziniu.1003@gmail.com>
2022-07-25 22:28:20 +0800

    debug_compare

---
+++ CSVCompTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class CSV_CompTest {
+
+    @Test
+    public void testRead() throws IOException {
+        CSV_Comp.read();
+    }
+}
--- CSV_Comp.java
+++ CSV_Comp.java
@@ -1,43 +1,96 @@
 import java.io.*;
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class CSV_Comp {
 
+    private final String path1;
+    private final String path2;
+    private final List<Integer> combination;
 
-    public static void main(String[] args) throws IOException {
-        List<String[]> output = new ArrayList<>();
-        //parsing a CSV file into Scanner class constructor
-        Scanner sc = new Scanner(new File("sample_file_1.csv"));
-        Scanner sc2 = new Scanner(new File("sample_file_3.csv"));
-        sc.useDelimiter("[\n]");   //sets the delimiter pattern
-        sc2.useDelimiter("[\n]");   //sets the delimiter pattern
 
-        while (sc.hasNext() && sc2.hasNext()) { //returns a boolean value
+    public CSV_Comp (String path1, String path2, List<Integer> combination) {
+        this.path1 = path1;
+        this.path2 = path2;
+        this.combination = combination;
+    }
 
-            String val1 = sc.next();
-            String val2 = sc2.next();
-            if (!Objects.equals(val1, val2)) {
-                String[] str1 = val1.split(",");
-                System.out.println(Arrays.toString(str1));
-                String[] str2 = val2.split(",");
-                System.out.println(Arrays.toString(str2));
-                output.add(str1);
-                output.add(str2);
-            }
+    public ArrayList<ArrayList<String>> read() throws FileNotFoundException {
+        Scanner sc = new Scanner(new File(path1));
+        Scanner sc2 = new Scanner(new File(path2));
+        sc.useDelimiter("\n");   //sets the delimiter pattern
+        ArrayList<String> str_arr1 = new ArrayList<>();
+        while (sc.hasNext()) {
+            str_arr1.add(sc.next());
+        }
+        sc2.useDelimiter("\n");   //sets the delimiter pattern
+        ArrayList<String> str_arr2 = new ArrayList<>();
+        while (sc2.hasNext()) {
+            str_arr2.add(sc2.next());
         }
         sc.close();
         sc2.close();
-        System.out.println();
-        System.out.println(output);
+        ArrayList<ArrayList<String>> output = new ArrayList<>();
+        output.add(str_arr1);
+        output.add(str_arr2);
+        return output;
+    }
 
-        File csvFile = new File("output.csv");
+    public ArrayList<String> compare() throws FileNotFoundException {
+        ArrayList<String> csv1 = read().get(0);
+        ArrayList<String> csv2 = read().get(1);
+        ArrayList<String> output = new ArrayList<>();
+        for (String s1 : csv1) {
+            String[] val1 = s1.split(",");
+            String s1_matched = "";
+            boolean found = false;
+            for (String s2 : csv2) {
+                String[] val2 = s2.split(",");
+                boolean matched = true;
+                for (int i : combination) {
+                    if (!Objects.equals(val1[i], val2[i])) {
+                        matched = false;
+                        break;
+                    }
+                }
+                if (matched) {
+                    found = true;
+                    s1_matched = s2;
+                    break;
+                }
+            }
+            if (!found) {
+                System.out.println(s1);
+                output.add(s1);
+            } else {
+                if (!Objects.equals(s1, s1_matched)) {
+                    System.out.println(s1);
+                    System.out.println(s1_matched);
+                    output.add(s1);
+                    output.add(s1_matched);
+                }
+                csv2.remove(s1_matched);
+            }
+        }
+        for (String remaining : csv2) {
+            System.out.println(remaining);
+            output.add(remaining);
+        }
+        return output;
+    }
+
+    public void write_CSV(String outputName) throws IOException {
+        ArrayList<String> unformatted = compare();
+        ArrayList<String[]> formatted = new ArrayList<>();
+        for (String line : unformatted) {
+            String[] newLine = line.split(",");
+            formatted.add(newLine);
+        }
+
+        File csvFile = new File(outputName);
         FileWriter fileWriter = new FileWriter(csvFile);
 
         //write header line here if you need.
-
-        for (String[] data : output) {
+        for (String[] data : formatted) {
             StringBuilder line = new StringBuilder();
             for (int i = 0; i < data.length; i++) {
 //                line.append("\"");
@@ -53,6 +106,5 @@ public class CSV_Comp {
             fileWriter.write(line.toString());
         }
         fileWriter.close();
-
     }
 }
